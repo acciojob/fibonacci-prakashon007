fbd38e7bbe96bebac805d74fcbbb34426019e199
Errolla Prakash <119421315+prakashon007@users.noreply.github.com>
2023-07-24 12:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function fibonacci(num) {
 // your code here
-	if(num==1){
-		return 0;
+	// if(num==1){
+	// 	return 0;
+	// }
+	// if(num==2){
+	// 	return 1;
+	// }
+	// return fibonacci(num-1)+fibonacci(num-2);
+
+	let a = 0;let b=1;
+	let c=0;
+	let ans =0;
+	for(let i=0;i<num;i++){
+		ans=a;
+		c=a+b;
+		b=c;
+		a=b;
 	}
-	if(num==2){
-		return 1;
-	}
-	return fibonacci(num-1)+fibonacci(num-2);
+	return ans;
 	
 }
 
